94e0c2a47d3c2cbc6f7e5eb9a621b0b3d1c49e42
Ana Carolina Kazue Inada <kazu@MacBook-Air-de-Ana.local>
2022-06-10 14:13:03 -0300

    setup

---
+++ teste-tecnico-shopper/src/globalStyle/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FFFFFF",
+      contrastText: "#03030B",
+    },
+    text: {},
+    secondary: {
+      light: "#E8F9F3",
+      main: "rgb(63, 173, 133)",
+      contrastText: "#FFFFFF",
+    },
+  },
+});
+export default theme;
---
+++ teste-tecnico-shopper/src/pages/index.js
@@ -0,0 +1,5 @@
+const Index = () => {
+  return <h1>intex</h1>;
+};
+
+export default Index;
